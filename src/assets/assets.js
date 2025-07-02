b51574600c48c96558435857b34b4008b3c6b460
malahatmrd <malahatmardani@gmail.com>
2025-07-02 15:54:20 +0430

    update faratan

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import menu from './menu.png';
-import headerPic from './headerPic.png';
-import Bodybuilding from './Bodybuilding.png';
-import aboutUsPic from './aboutUsPic.png';
-import lin1 from './lin1.png';
-import lin2 from './lin2.png';
-import crown from './crown.png';
-import bottompic from './bottompic.png';
-import instagram from './instagram.png';
-import instagramSVG from './instagram.svg';
-import back from './back.png';
-import loginpic from './loginpic.png';
-import signuppic from './signuppic.png';
-import freepik from './freepik.png';
-import Verifypic from './Verifypic.png';
-import welcompic from './welcompic.png';
-import upload from './upload.png';
-import bag from './bag.png';
-import forgetpic from './forgetpic.svg';     // ❗ بررسی وجود فایل
-import newpasspic from './newpasspic.svg';   // ❗ بررسی وجود فایل
-
-export const assets = {
-  menu,
-=======
 import menu from "./menu.png";
 import headerPic from "./headerPic.png";
 import Bodybuilding from "./Bodybuilding.png";
@@ -33,18 +8,19 @@ import crown from "./crown.png";
 import bottompic from "./bottompic.png";
 import instagram from "./instagram.png";
 import instagramSVG from "./instagram.svg";
-import BodybuildingPic from "./BodybuildingPic.png";
-import woman from "./woman.png";
-import woman1 from "./woman1.png";
-import woman2 from "./woman2.png";
-import woman3 from "./woman3.png";
-import woman4 from "./woman4.png";
-import Profile from "./Profile.png";
+import back from "./back.png";
+import loginpic from "./loginpic.png";
+import signuppic from "./signuppic.png";
+import freepik from "./freepik.png";
+import Verifypic from "./Verifypic.png";
+import welcompic from "./welcompic.png";
+import upload from "./upload.png";
+import bag from "./bag.png";
+import forgetpic from "./forgetpic.svg";
+import newpasspic from "./newpasspic.svg";
 
 export const assets = {
   menu,
-  Profile,
->>>>>>> fc21a90 (update faratan)
   headerPic,
   Bodybuilding,
   aboutUsPic,
@@ -54,7 +30,6 @@ export const assets = {
   bottompic,
   instagram,
   instagramSVG,
-<<<<<<< HEAD
   back,
   loginpic,
   signuppic,
@@ -65,12 +40,4 @@ export const assets = {
   bag,
   forgetpic,
   newpasspic,
-=======
-  BodybuildingPic,
-  woman,
-  woman1,
-  woman2,
-  woman3,
-  woman4,
->>>>>>> fc21a90 (update faratan)
 };
