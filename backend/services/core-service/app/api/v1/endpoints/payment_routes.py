4016cbb89ddb9d8e4413dd10fc33c13c7e9704f3
qazalnaderi <ghazalnaderi4@gmail.com>
2025-07-19 01:19:35 +0330

    Zarinpal gateway integration and session purchase created

---
+++ backend/services/core-service/app/api/v1/endpoints/payment_routes.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+from app.services.payment_main_service import PaymentMainService
+from fastapi import APIRouter, Depends, Query
+from uuid import UUID
+from app.services.auth_services.auth_service import get_current_member
+from app.domain.schemas.token_schema import TokenDataSchema
+from loguru import logger
+
+payment_router = APIRouter()
+
+@payment_router.post("/payment-request")
+async def request_payment(
+    session_id: UUID,
+    current_member: Annotated[TokenDataSchema, Depends(get_current_member)],
+    payment_main_service: Annotated[PaymentMainService, Depends()] ,
+):
+    logger.info(f"member {current_member.id} is starting to pay...")
+    payment_url = await payment_main_service.initiate_payment(current_member.id, session_id)
+    return {"payment_url": payment_url}
+
+
+@payment_router.get("/callback/{purchase_id}")
+async def payment_callback(
+    payment_main_service: Annotated[PaymentMainService, Depends()],
+    purchase_id: UUID,
+    authority: str = Query(..., alias="Authority"),  
+    status: str = Query(..., alias="Status"),
+):
+    return await payment_main_service.confirm_payment(purchase_id, authority, status)
+
